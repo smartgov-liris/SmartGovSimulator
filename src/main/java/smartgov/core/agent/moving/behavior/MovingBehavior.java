bd8e75dcaf752cf59b77e754f75a8d65db2aab8b
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-06-05 08:48:14 +0200

    Include plan update in refresh()

--- src/main/java/smartgov/core/agent/moving/behavior/MovingBehavior.java
+++ src/main/java/smartgov/core/agent/moving/behavior/MovingBehavior.java
@@ -1,23 +1,28 @@
 package smartgov.core.agent.moving.behavior;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import smartgov.core.agent.core.AgentBody;
 import smartgov.core.agent.core.behavior.Behavior;
+import smartgov.core.agent.moving.MovingAgentBody;
+import smartgov.core.environment.SmartGovContext;
 import smartgov.core.environment.graph.node.Node;
 import smartgov.core.output.node.NodeIdSerializer;
 
 public abstract class MovingBehavior extends Behavior<MoverAction> {
-
+	@JsonIgnore
+	private SmartGovContext context;
+	
 	@JsonSerialize(using=NodeIdSerializer.class)
 	private Node origin;
 	@JsonSerialize(using=NodeIdSerializer.class)
 	private Node destination;
 	
-	public MovingBehavior(AgentBody<MoverAction> agentBody, Node origin, Node destination) {
+	public MovingBehavior(MovingAgentBody agentBody, Node origin, Node destination, SmartGovContext context) {
 		super(agentBody);
 		this.origin = origin;
 		this.destination = destination;
+		this.context = context;
 	}
 
 	public Node getOrigin() {
@@ -31,6 +36,9 @@ public abstract class MovingBehavior extends Behavior<MoverAction> {
 	public void refresh(Node origin, Node destination) {
 		this.origin = origin;
 		this.destination = destination;
+		
+		// Update agents plan
+		((MovingAgentBody) getAgentBody()).updatePlan(context.graph.shortestPath(origin, destination));
 	}
 
 }
