c77cc97138308f4f346d1806c18f0f4566635d1c
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-08-21 16:07:35 +0200

    Improved Projection and LatLon docs

--- src/main/java/smartgov/urban/geo/utils/LatLon.java
+++ src/main/java/smartgov/urban/geo/utils/LatLon.java
@@ -7,20 +7,47 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
+/**
+ * Class used to represent geographical coordinates.
+ *
+ */
 @JsonSerialize(using = LatLon.Serializer.class)
 public class LatLon {
 
+	/**
+	 * Latitude in degree
+	 */
 	public double lat;
+	/**
+	 * Longitude in degree
+	 */
 	public double lon;
 
+	/**
+	 * LatLon constructor.
+	 * 
+	 * @param lat latitude
+	 * @param lon longitude
+	 */
 	public LatLon(double lat, double lon) {
 		super();
 		this.lat = lat;
 		this.lon = lon;
 	}
 	
+	/**
+	 * Computes the distance in meter from point 1 to point 2 using the Haversine formula.
+	 * 
+	 * <p>
+	 * Alias for {@link Projection#haversineDistance(LatLon, LatLon)}.
+	 * </p>
+	 * 
+	 * @param point1 first geographical point
+	 * @param point2 second geographical point
+	 * @return distance in meter
+	 */
 	public static double distance(LatLon point1, LatLon point2) {
-		return Projection.haversineDistance(point1.lat, point2.lat, point1.lon, point2.lon);
+		return Projection.haversineDistance(point1, point2);
 	}
 
 	@Override
--- src/main/java/smartgov/urban/geo/utils/Projection.java
+++ src/main/java/smartgov/urban/geo/utils/Projection.java
@@ -3,24 +3,36 @@ package smartgov.urban.geo.utils;
 import org.locationtech.jts.geom.Coordinate;
 
 public interface Projection {
+	
+	/**
+	 * Mean earth radius used in the {@link #haversineDistance haversineDistance}, computed from the 
+	 * WGS84 coordinate system, also used by Open Street Map. Notice that this system
+	 * actually use an ellipsoid to represent the earth, and so this "mean" distance
+	 * actually correspond to the average value obtained from semi-minor and semi-major
+	 * axis defined in the system.
+	 */
+	public static final double MEAN_EARTH_RADIUS = 6371009;
 
 	public Coordinate project(LatLon geoPoint);
 	public LatLon unproject(Coordinate point);
 	
-	public static double euclidianDistance(double x1, double x2, double y1, double y2) {
-		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-	}
-	
-	public static double haversineDistance(double lat1, double lat2, double lon1, double lon2) {
+	/**
+	 * Computes the Haversine distance from the specified latitudes and longitudes, using
+	 * the {@link #MEAN_EARTH_RADIUS} defined in this class.
+	 * 
+	 * @param point1 first geographical point
+	 * @param point2 second geographical point
+	 * @return distance in meter
+	 */
+	public static double haversineDistance(LatLon point1, LatLon point2) {
 		//haversine formula https://en.wikipedia.org/wiki/Haversine_formula
-			double earthRadius = 6371000;
-			double _lat1 = Math.toRadians(lat1);
-			double _lon1 = Math.toRadians(lon1);
-			double _lat2 = Math.toRadians(lat2);
-			double _lon2 = Math.toRadians(lon2);
+			double _lat1 = Math.toRadians(point1.lat);
+			double _lon1 = Math.toRadians(point1.lon);
+			double _lat2 = Math.toRadians(point2.lat);
+			double _lon2 = Math.toRadians(point2.lon);
 			double a = Math.pow(Math.sin((_lat1 - _lat2)/2),2) + Math.cos(_lat1)*Math.cos(_lat2)*Math.pow(Math.sin((_lon1-_lon2)/2),2);
 			double c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-			double d = earthRadius*c;
+			double d = MEAN_EARTH_RADIUS * c;
 			return d;
 	}
 }
