df25eb871e5ae990789aac796d56d6d3cf199588
vdeslandres <veronique.deslandres@univ-lyon1.fr>
2019-08-28 11:46:40 +0200

    Updated Windows incompatible test

--- src/test/java/org/liris/smartgov/simulator/core/utils/TestFiles.java
+++ src/test/java/org/liris/smartgov/simulator/core/utils/TestFiles.java
@@ -3,6 +3,8 @@ package org.liris.smartgov.simulator.core.utils;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.File;
+
 import org.junit.Test;
 import org.liris.smartgov.simulator.core.environment.TestContext;
 import org.liris.smartgov.simulator.core.environment.TestSmartGovContext;
@@ -13,12 +15,14 @@ public class TestFiles {
 	public void testLoadFileFromConfigFile() {
 		TestContext context = TestSmartGovContext.loadTestContext();
 		
+		String loadedPath = (String) context.getFileLoader().load("outputFolder").getAbsolutePath();
+		String expectedPath = new File(TestSmartGovContext.class.getResource("output").getFile()).getAbsolutePath();
 		/*
 		 * Loaded output folder should be relative to the config file.
 		 */
 		assertThat(
-				(String) context.getFileLoader().load("outputFolder").getAbsolutePath(),
-				equalTo(TestSmartGovContext.class.getResource("output").getFile())
+				loadedPath,
+				equalTo(expectedPath)
 				);
 	}
 }
